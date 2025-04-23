92910b2cbe4b79d40b409ffd94d4f8dd6cc108f3
HoucemChhaidar <houcem.chhaidar@gmail.com>
2025-04-23 10:03:30 +0100

    first commit

---
+++ src/main/java/com/asmtunis/credix/backend/CredixBackendApplication.java
@@ -0,0 +1,11 @@
+package com.asmtunis.credix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredixBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CredixBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/asmtunis/credix/backend/CredixBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.asmtunis.credix.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredixBackendApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
